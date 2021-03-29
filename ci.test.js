7e4f92e1861d1d082e877df992d70e3f4cadfb6d
Prima Aulia Gusta <primaulia@gmail.com>
2021-03-29 20:47:30 +0800

    passing the test

--- ci.test.js
+++ ci.test.js
@@ -1,18 +1,46 @@
-var fs = require('fs'); 
+var fs = require("fs");
 
-describe('CI sequence', () => {
-
+describe("CI sequence", () => {
   var fileContents;
   beforeEach(() => {
-    fileContents = fs.readFileSync('ci.md', 'utf8');
+    fileContents = fs.readFileSync("ci.md", "utf8");
   });
 
-  it('the list has a header', () => {
-    expect(/.*#.*/ig.test(fileContents)).toBe(true);
+  it("the list has a header", () => {
+    expect(/.*#.*/gi.test(fileContents)).toBe(true);
   });
 
   // TODO add the tests between these comments =>
+  it("1. pull latest code", () => {
+    expect(/.*pull.*/gi.test(fileContents)).toBe(true);
+  });
 
+  it("2. add commits", () => {
+    expect(/.*commit.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it("3. push to the remote branch with the same name", () => {
+    expect(/.*push.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it("3. push to the remote branch with the same name", () => {
+    expect(/.*push.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it("4. open a pull request and continue working", () => {
+    expect(/.*pull\s+request.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it("5. Merge/rebase commits from master. Make tests pass on the merge result.", () => {
+    expect(/.*merge.*commits.*tests\s+pass.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it("6. Deploy from the feature branch to production.", () => {
+    expect(/.*Deploy.*to\s+production.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it("7. If everything is good in production for some period of time, merge changes to master.", () => {
+    expect(/.*merge.*to\s+master.*/gi.test(fileContents)).toBe(true);
+  });
   // TODO <= add the tests between these comments
-
 });
